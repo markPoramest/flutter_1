279b455ead48ac129d8d701b62832baf297980c4
MarkKung <2411kjn@gmail.com>
2020-05-18 06:44:12 +0700

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
+  void onPressAbhisij() {
+    print("Wrong");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(
-        title: Text('My first App'),
-      ),
-        body: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Who is PM of Thailand?'),
+            RaisedButton(
+              child: Text('Prayuth Chan-o-cha'),
+              onPressed: () => print('Correct'),
+            ),
+            RaisedButton(
+                child: Text('Yingluck Shinawatra'),
+                onPressed: () {
+                  print("Wrong");
+                }),
+            RaisedButton(
+              child: Text('Abhisit Vejjajiva'),
+              onPressed: onPressAbhisij,
+            ),
+            RaisedButton(
+              child: Text('Suthep Thaugsuban'),
+              onPressed: () => print("Wrong"),
+            )
+          ],
+        ),
       ),
     );
   }
-  
 }
