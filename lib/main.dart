6d82c2b4f72ca20d176b02aac1975704db413d6c
MarkKung <2411kjn@gmail.com>
2020-05-18 06:22:34 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(
+        title: Text('My first App'),
+      ),
+        body: Text('Hello World'),
+      ),
+    );
+  }
+  
+}
